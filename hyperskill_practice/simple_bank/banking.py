795b2f4ac799d17a1e6155e982875a444dd2d836
Mariia <sorokmash8b@gmail.com>
2021-06-16 11:34:57 +0300

    small changes

--- hyperskill_practice/simple_bank/banking.py
+++ hyperskill_practice/simple_bank/banking.py
@@ -1,53 +1,33 @@
 from random import randint as ra
 import sqlite3
 
-"""
-А если все в одном классе то это очень локально и легко исправить
- 
-И второе
- 
-Это абстракция
- 
-Типо странно что банк знает про такие низкоуровневые детали как запросы sql
- 
-В том и смысл оопе
- 
-Ты разделяешь абстракции на разные уровни
- 
-И не экономь на именах переменнхы
- 
-con, cur и так далее это так себе имена
-
-db_connection уже получше
- 
-Но main точно должен быть отдельно
- 
-Типо класс банк отвечает за работу с аккаунтами
- 
-Он не должен внутри себя крутить цикл с вводом/выводом команд
- 
-Кстати, хорошая идея попробовать написать немного тестов на эту прогу
-"""
-
-
-
 
 class Bank:
 
     def __init__(self, db_file="card.s3db"):
         try:
-            self.con = sqlite3.connect(db_file)
+            self.connection_db = sqlite3.connect(db_file)
         except FileNotFoundError:
-            file = open("card.s3db", "w")
-            self.con = sqlite3.connect(file)
-        self.cur = self.con.cursor()
-        self.cur.execute("CREATE TABLE IF NOT EXISTS card (id INTEGER UNIQUE,number TEXT UNIQUE,pin TEXT,balance INTEGER DEFAULT 0);")
-        self.con.commit()
-        self.cur.execute('select max(id) from card')
-        self.new_id = self.cur.fetchone()[0]
-        self.cur_acc = None
+            try:
+                file = open("card.s3db", "w")
+                self.connection_db = sqlite3.connect(file)
+            except PermissionError:
+                print("You don't have permission to create files in this directory")
+        self.cursor = self.connection_db.cursor()
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS card (id INTEGER UNIQUE,number TEXT UNIQUE,pin TEXT,balance INTEGER DEFAULT 0);")
+        self.connection_db.commit()
+        self.cursor.execute('select max(id) from card')
+        self.new_id = self.cursor.fetchone()[0]
+        self.current_account = None
 
-    def luhn_check(self,number):
+    @staticmethod
+    def luhn_check(number):
+        """
+        Алгоритм Луна использует простое правило вычисления контрольной суммы для проверки идентификационных номеров,
+        таких как номера пластиковых карт.
+        Алгоритм определяет ошибки ввода одной неправильной цифры,
+        а также почти все перестановки соседних цифр, за исключением перестановки 09-90 или обратной 90-09.
+        """
         card = list(number)
         card = [int(i) for i in card]
         last_digit = card[-1]
@@ -66,14 +46,14 @@ class Bank:
 
     def create_account(self):
         acc = Account()
-        self.cur.execute('select max(id) from card')
-        self.new_id = self.cur.fetchone()[0]
+        self.cursor.execute('select max(id) from card')
+        self.new_id = self.cursor.fetchone()[0]
         if self.new_id is None:
             self.new_id = 0
-        ins = "insert into card values({l[0]},{l[1]},{l[2]},{l[3]});".format(
+        insert_query = "insert into card values({l[0]},{l[1]},{l[2]},{l[3]});".format(
             l=[str((self.new_id + 1)), str(acc.card_number), str(acc.pin), str(acc.balance)])
-        self.cur.execute(ins)
-        self.con.commit()
+        self.cursor.execute(insert_query)
+        self.connection_db.commit()
         print("Your card has been created")
         print("Your card number:\n{}".format(acc.card_number))
         print("Your card PIN:\n{}\n".format(acc.pin))
@@ -82,14 +62,14 @@ class Bank:
         card_input = input("Enter your card number:")
         pin_input = input("Enter your PIN:")
         print("\n")
-        check = "select * from card where number = {}".format(card_input)
-        self.cur.execute(check)
-        query = self.cur.fetchall()
+        check_query = "select * from card where number = {}".format(card_input)
+        self.cursor.execute(check_query)
+        query = self.cursor.fetchall()
         if query:
             for i in query:
                 if i[1] == card_input:
                     if i[2] == pin_input:
-                        self.cur_acc = card_input
+                        self.current_account = card_input
                         print("You have successfully logged in!\n")
                     else:
                         print("Wrong card number or PIN!\n")
@@ -99,19 +79,19 @@ class Bank:
             print("Wrong card number or PIN!\n")
 
     def logout(self):
-        self.cur_acc = None
+        self.current_account = None
 
     def check_balance(self):
-        if self.cur_acc is not None:
-            check = "select * from card where number = {}".format(self.cur_acc)
-            self.cur.execute(check)
-            query = self.cur.fetchall()[0]
+        if self.current_account is not None:
+            check_query = "select * from card where number = {}".format(self.current_account)
+            self.cursor.execute(check_query)
+            query = self.cursor.fetchall()[0]
             print("Balance:{}\n".format(query[3]))
 
     def get_balance(self, cardb):
-        check = "select * from card where number = {}".format(cardb)
-        self.cur.execute(check)
-        query = self.cur.fetchall()
+        check_query = "select * from card where number = {}".format(cardb)
+        self.cursor.execute(check_query)
+        query = self.cursor.fetchall()
         res = None
         if query is not []:
             res = query[0][3]
@@ -121,54 +101,50 @@ class Bank:
         return res
 
     def add_income(self):
-        if self.cur_acc is not None:
-            cur_bal = self.get_balance(self.cur_acc)
+        if self.current_account is not None:
+            cur_bal = self.get_balance(self.current_account)
             inc = input("Enter income:\n")
-            upd = "update card set balance = {l[0]} where number = {l[1]}".format(l=[cur_bal + int(inc), self.cur_acc])
-            self.cur.execute(upd)
-            self.con.commit()
+            update_query = "update card set balance = {l[0]} where number = {l[1]}".format(l=[cur_bal + int(inc), self.current_account])
+            self.cursor.execute(update_query)
+            self.connection_db.commit()
             print("Income was added!\n")
 
     def do_transfer(self):
         print("Transfer")
         trans_number = input("Enter card number:")
-        if self.luhn_check(trans_number):
-            check = "select * from card where number = {}".format(trans_number)
-            self.cur.execute(check)
-            d = self.cur.fetchall()
-            #print(d)
-            if not d:
+        check_query = "select * from card where number = {}".format(trans_number)
+        self.cursor.execute(check_query)
+        checking_card = self.cursor.fetchall()
+        if not checking_card:
                 print("Such a card does not exist.\n")
-            else:
-                mo = input("Enter how much money you want to transfer:")
-                cur_bal1 = self.get_balance(self.cur_acc)
-                if int(mo) > cur_bal1:
-                    print("Not enough money!\n")
-                else:
-                    cur_bal1 = cur_bal1 - int(mo)
-                    upd = "update card set balance = {l[0]} where number = {l[1]};".format(l=[cur_bal1, self.cur_acc])
-                    self.cur.execute(upd)
-                    self.con.commit()
-                    cur_bal2 = d[0][3] + int(mo)
-                    upd_t = "update card set balance = {l[0]} where number = {l[1]};".format(l=[cur_bal2, trans_number])
-                    self.cur.execute(upd_t)
-                    self.con.commit()
-                    print("Success!\n")
-
         else:
-            print("Probably you made a mistake in the card number. Please try again!\n")
+            money = input("Enter how much money you want to transfer:")
+            current_balance1 = self.get_balance(self.current_account)
+            if int(money) > current_balance1:
+                print("Not enough money!\n")
+            else:
+                current_balance1 = current_balance1 - int(money)
+                update_query1 = "update card set balance = {l[0]} where number = {l[1]};".format(l=[current_balance1, self.current_account])
+                self.cursor.execute(update_query1)
+                self.connection_db.commit()
+                current_balance2 = checking_card[0][3] + int(money)
+                update_query2 = "update card set balance = {l[0]} where number = {l[1]};".format(l=[current_balance2, trans_number])
+                self.cursor.execute(update_query2)
+                self.connection_db.commit()
+                print("Success!\n")
+
 
     def close_acc(self):
-        dele = "delete from card where number = {}".format(self.cur_acc)
-        self.cur.execute(dele)
-        self.con.commit()
-        self.cur_acc = None
+        delete_query = "delete from card where number = {}".format(self.current_account)
+        self.cursor.execute(delete_query)
+        self.connection_db.commit()
+        self.current_account = None
         print("The account has been closed!\n")
 
     def main(self):
         exit_bool = True
         while exit_bool:
-            if self.cur_acc is None:
+            if self.current_account is None:
                 print("1. Create an account\n2. Log into account\n0. Exit")
                 answer = input()
                 if answer == "1":
@@ -196,10 +172,20 @@ class Bank:
 
 
 class Account(Bank):
-    def luhn(self):
+    @staticmethod
+    def luhn():
+        """
+        Алгоритм Луна использует простое правило вычисления контрольной суммы для проверки идентификационных номеров,
+        таких как номера пластиковых карт.
+        Алгоритм определяет ошибки ввода одной неправильной цифры,
+        а также почти все перестановки соседних цифр, за исключением перестановки 09-90 или обратной 90-09.
+
+        В данном случае в задании было указано генерировать номера карт
+        с 400000 + случайные цифры(длина номера карты 16), которые должны проходить проверку
+        алгоритмом Луна.
+        """
         card = str("400000" + "".join([str(ra(0, 9)) for _ in range(10)]))
         card = [int(i) for i in card]
-        last_digit = 0
         rest = card[:len(card) - 1]
         for i in range(len(rest)):
             if i % 2 == 0:
@@ -208,6 +194,7 @@ class Account(Bank):
             if rest[i] > 9:
                 rest[i] = rest[i] - 9
         s = sum(rest)
+        last_digit = 0
         if s % 10 == 0:
             last_digit = 0
         else:
